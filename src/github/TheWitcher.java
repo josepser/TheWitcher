631c90d7702ac9a154b082ca48f9e5e4e3ebbb45
josep <josepserranora@gmail.com>
2023-01-11 12:40:04 +0100

    añadidos atributos

--- src/github/TheWitcher.java
+++ src/github/TheWitcher.java
@@ -1,10 +1,11 @@
 package github;
 
 public class TheWitcher {
-
+	private String name;
+	private int health;
+	private boolean hasSoul;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 }
