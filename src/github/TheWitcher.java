13fc31d6585c103c67a605e943c0a68cf3163b52
josep <josepserranora@gmail.com>
2023-01-11 13:40:07 +0100

    metodo isDeath

--- src/github/TheWitcher.java
+++ src/github/TheWitcher.java
@@ -4,26 +4,50 @@ public class TheWitcher {
 	private String name;
 	private int health;
 	private boolean hasSoul;
-	public static void main(String[] args) {
-		
-	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public int getHealth() {
 		return health;
 	}
+
 	public void setHealth(int health) {
 		this.health = health;
 	}
+
 	public boolean isHasSoul() {
 		return hasSoul;
 	}
+
 	public void setHasSoul(boolean hasSoul) {
 		this.hasSoul = hasSoul;
 	}
 
+	public boolean isDeath() {
+		if (health == 0) {
+			return true;
+		} else if (!hasSoul) {
+			return false;
+		} else {
+			return false;
+		}
+	}
+
+	public static void main(String[] args) {
+		TheWitcher brujo = new TheWitcher();
+		brujo.setName("Geralt de Rivia");
+		brujo.setHealth(1);
+		brujo.setHasSoul(true);
+		if(brujo.isDeath()) {
+			System.out.println(brujo.getName()+" está muerto");
+		}else {
+			System.out.println(brujo.getName()+" está vivo");
+		}
+	}
 }
