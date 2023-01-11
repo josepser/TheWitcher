ecfa7f2d81cea45d725de6d338332c7529de727a
josep <josepserranora@gmail.com>
2023-01-11 12:47:40 +0100

    Añadidos getters i setters

--- src/github/TheWitcher.java
+++ src/github/TheWitcher.java
@@ -7,5 +7,23 @@ public class TheWitcher {
 	public static void main(String[] args) {
 		
 	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getHealth() {
+		return health;
+	}
+	public void setHealth(int health) {
+		this.health = health;
+	}
+	public boolean isHasSoul() {
+		return hasSoul;
+	}
+	public void setHasSoul(boolean hasSoul) {
+		this.hasSoul = hasSoul;
+	}
 
 }
